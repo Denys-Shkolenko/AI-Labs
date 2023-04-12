208ef713ab6d62cdb52b9bbd7129df787edb59bf
Denys-Shkolenko <denys.shkolenko@gmail.com>
2023-04-12 18:00:58 +0300

    feat: network with exceptions

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,5 +1,12 @@
 from neuron import Neuron
-
+from network import Network
 
 if __name__ == "__main__":
-    neuron = Neuron(1.2)
+    init_weights_for_hidden_layer = ((w14, w15, w16),
+                                     (w24, w25, w26),
+                                     (w34, w35, w36)) = ((1.0, 1.0, 1.0),
+                                                         (1.0, 1.0, 1.0),
+                                                         (1.0, 1.0, 1.0))
+
+    init_weights_for_output_neuron = (w47, w57, w67) = (1.0, 1.0, 1.0)
+
--- Lab1/network.py
+++ Lab1/network.py
@@ -5,8 +5,18 @@ from neuron import Neuron
 
 class Network:
 
-    def __init__(self, input_layer: Sequence[Neuron], expected_value: float):
+    def __init__(self, input_layer: Sequence[Neuron],
+                 init_weights_for_hidden_layer: Sequence[Sequence[float]],
+                 init_weights_for_output_neuron: Sequence[float],
+                 expected_value: float,
+                 hidden_layer_quantity=3,
+                 learning_rate=0.1):
+        self.hidden_layer_quantity = hidden_layer_quantity
+        self.learning_rate = learning_rate
+
         self.input_layer = input_layer
+        self.weights_for_hidden_layer = init_weights_for_hidden_layer
+        self.weights_for_output_neuron = init_weights_for_output_neuron
         self.expected_value = expected_value
 
     @property
@@ -20,6 +30,38 @@ class Network:
         self.__layer_1 = layer_1
 
     @property
+    def weights_for_hidden_layer(self):
+        return self.__weights_for_hidden_layer
+
+    @weights_for_hidden_layer.setter
+    def weights_for_hidden_layer(self, weights_for_hidden_layer):
+        if len(weights_for_hidden_layer) != len(self.input_layer):
+            raise ValueError("there must be weights for each neuron from "
+                             "the input layer")
+        for sequence in weights_for_hidden_layer:
+            if len(sequence) != self.hidden_layer_quantity:
+                raise ValueError("there must be weights for each neuron from "
+                                 "the hidden layer")
+            for weight in sequence:
+                if not isinstance(weight, float):
+                    raise TypeError("each weight must be a float")
+        self.__weights_for_hidden_layer = weights_for_hidden_layer
+
+    @property
+    def weights_for_output_neuron(self):
+        return self.__weights_for_output_neuron
+
+    @weights_for_output_neuron.setter
+    def weights_for_output_neuron(self, weights_for_output_neuron):
+        if len(weights_for_output_neuron) != self.hidden_layer_quantity:
+            raise ValueError("there must be weights for each neuron from "
+                             "the hidden layer")
+        for weight in weights_for_output_neuron:
+            if not isinstance(weight, float):
+                raise TypeError("each weight must be a float")
+        self.__weights_for_output_neuron = weights_for_output_neuron
+
+    @property
     def expected_value(self):
         return self.__expected_value
 
@@ -28,3 +70,5 @@ class Network:
         if not isinstance(expected_value, float):
             raise TypeError("expected_value must be a float")
         self.__expected_value = expected_value
+
+    # def start_training(self):
