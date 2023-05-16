eb13e37db3e7a33c82329f297884ee0d544a4df1
Denys-Shkolenko <denys.shkolenko@gmail.com>
2023-05-16 21:53:53 +0300

    feat: new training method

--- Lab2/main.py
+++ Lab2/main.py
@@ -11,30 +11,19 @@ if __name__ == "__main__":
                       size_of_output_layer=3,
                       default_weight=0.1,
                       learning_rate=0.1,
-                      eps=0.001,
-                      max_iterations=100_000,
+                      eps=0.0001,
+                      max_iterations=1,
                       activation_func=derivative_err_func)
 
     with open("training_data.json") as training_data:
         training_data = json.load(training_data)
 
     # training
-    table = []
-    print("*" * 20, "TRAINING", "*" * 20)
-    for data in training_data:
-        network.set_input_data(data["symbol"])
-        network.set_expected_values(data["8-bit code"])
-        iterations = network.start_training()
-
-        table.append([
-            data["answer"],
-            ''.join(str(num) for num in data["8-bit code"]),
-            '   '.join(str(round(num, 8)) for num in network.get_y()),
-            iterations
-        ])
-
-    print(tabulate(table, headers=["Image", "Expected Value", "y", "Iterations"], tablefmt="orgtbl"))
-    print("\n")
+    for _ in range(1000):
+        for data in training_data:
+            network.set_input_data(data["symbol"])
+            network.set_expected_values(data["8-bit code"])
+            iterations = network.start_training()
 
     # testing
     table = []
@@ -46,7 +35,7 @@ if __name__ == "__main__":
         table.append([
             data["answer"],
             ''.join(str(num) for num in data["8-bit code"]),
-            ''.join(str(round(num)) for num in network.get_y())
+            ''.join(str(round(num - 0.1)) for num in network.get_y())
         ])
 
     print(tabulate(table, headers=["Image", "Expected Value", "y", "Iterations"], tablefmt="orgtbl"))
