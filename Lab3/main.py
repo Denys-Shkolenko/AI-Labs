b9ab9aea86022250b7bcfda3654bcfabdba2ffd9
Denys-Shkolenko <denys.shkolenko@gmail.com>
2023-05-24 00:19:05 +0300

    lab3

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,10 +1,7 @@
-import math
-import random
 import matplotlib.pyplot as plt
 
-
-def evaluate_fitness(x):
-    return 5 * math.sin(x) * math.cos((x**2) + (1 / x))**2
+import random
+import math
 
 
 def genetic_algorithm():
@@ -30,7 +27,54 @@ def genetic_algorithm():
             worst_fitness = min(fitness)
             worst_solution = population[fitness.index(min(fitness))]
 
+        # Селекція
+        selected_parents = selection(population, fitness)
+
+        # Схрещування
+        offspring = crossover(selected_parents)
+
+        # Мутація
+        mutated_offspring = mutation(offspring)
+
+        # Заміна популяції новими індивідами
+        population = mutated_offspring
+
     return best_solution, best_fitness, worst_solution, worst_fitness, fitness_history
+
+
+def selection(population, fitness):
+    total_fitness = sum(fitness)
+    probabilities = [f / total_fitness for f in fitness]
+    selected_parents = random.choices(population, weights=probabilities, k=len(population))
+    return selected_parents
+
+
+def crossover(parents):
+    offspring = []
+    for i in range(0, len(parents), 2):
+        parent1 = parents[i]
+        parent2 = parents[i+1]
+        child = (parent1 + parent2) / 2.0
+        offspring.append(child)
+    return offspring
+
+
+def mutation(offspring):
+    mutated_offspring = []
+    mutation_rate = 0.1
+    for child in offspring:
+        if random.random() < mutation_rate:
+            mutated_child = child + random.uniform(-0.5, 0.5)
+            mutated_offspring.append(mutated_child)
+        else:
+            mutated_offspring.append(child)
+    return mutated_offspring
+
+
+def evaluate_fitness(individual):
+    x = individual
+    fitness = 5 * math.sin(x) * math.cos((x**2) + (1 / x))**2
+    return fitness
 
 
 if __name__ == "__main__":
@@ -41,11 +85,6 @@ if __name__ == "__main__":
     print("Найкращий фітнес (максимум):", best_fitness)
     print("Найгірше рішення (мінімум):", worst_solution)
     print("Найгірший фітнес (мінімум):", worst_fitness)
-
-    # plt.plot(fitness_history)
-    # plt.xlabel("Покоління")
-    # plt.ylabel("Фітнес")
-    # plt.show()
 
     x_values = [x / 100 for x in range(100 * 1, 100 * 11)]
     y_values = [evaluate_fitness(x) for x in x_values]
